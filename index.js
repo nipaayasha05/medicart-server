c79d4d0d26d817f417cc76a3320f771ade6cf8e4
nipaayasha05 <nipaayasha05@gmail.com>
2025-07-18 21:52:03 +0600

    implement jwt

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
@@ -9,8 +11,14 @@ const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
 // DsiyMUjOCfTiWmLY
 //medicine-selling
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgjw94w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,6 +31,21 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access" });
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      // console.log(err);
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+  // console.log(token);
+};
 
 async function run() {
   try {
@@ -39,6 +62,48 @@ async function run() {
     const paymentCollection = db.collection("payment");
     const paymentCompleteCollection = db.collection("payment-complete");
     const categoryCollection = db.collection("category");
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req?.user?.email;
+      const user = await usersCollection.findOne({ email });
+      if (!user || user?.role !== "Admin")
+        return res.status(403).send({ message: "Admin only " });
+      next();
+    };
+
+    const verifySeller = async (req, res, next) => {
+      const email = req?.user?.email;
+      const user = await usersCollection.findOne({ email });
+      if (!user || user?.role !== "Seller")
+        return res.status(403).send({ message: "Seller only" });
+      next();
+    };
+
+    app.post("/jwt", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.JWT_SECRET_KEY === "production",
+          sameSite:
+            process.env.JWT_SECRET_KEY === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.JWT_SECRET_KEY === "production",
+          sameSite: process.JWT_SECRET_KEY === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     app.post("/user", async (req, res) => {
       const userData = req.body;
@@ -94,7 +159,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/getMedicine", async (req, res) => {
+    app.get("/getMedicine", verifyToken, verifySeller, async (req, res) => {
       const email = req.query.email;
       const result = await medicinesCollection
         .find({ email })
@@ -110,16 +175,34 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/getAdvertisement", async (req, res) => {
-      const email = req.query.email;
-      const result = await medicinesAdvertisement
-        .find({ email })
-        .sort({ createdAt: -1 })
-        .toArray();
-      res.send(result);
-    });
+    app.get(
+      "/getAdvertisement",
+      verifyToken,
+      verifySeller,
+      async (req, res) => {
+        const email = req.query.email;
+        const result = await medicinesAdvertisement
+          .find({ email })
+          .sort({ createdAt: -1 })
+          .toArray();
+        res.send(result);
+      }
+    );
 
-    app.get("/getAdminAdvertise", async (req, res) => {
+    app.get(
+      "/getAdminAdvertise",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const result = await medicinesAdvertisement
+          .find()
+          .sort({ createdAt: -1 })
+          .toArray();
+        res.send(result);
+      }
+    );
+
+    app.get("/getHomeAdvertise", async (req, res) => {
       const result = await medicinesAdvertisement
         .find()
         .sort({ createdAt: -1 })
@@ -127,7 +210,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/get-add-to-cart", async (req, res) => {
+    app.get("/get-add-to-cart", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const result = await cartCollection
+        .find({ userEmail: email })
+        .sort({ addedAt: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/get-add-to-cart-shop", async (req, res) => {
       const email = req.query.email;
       const result = await cartCollection
         .find({ userEmail: email })
@@ -144,88 +236,111 @@ async function run() {
     //   res.send(result);
     // });
 
-    app.get("/checkout/:id", async (req, res) => {
+    app.get("/checkout/:id", verifyToken, async (req, res) => {
       const { id } = req.params;
       const result = await paymentCollection.findOne({ _id: new ObjectId(id) });
 
       res.send(result);
     });
 
-    app.get("/invoice/:id", async (req, res) => {
+    app.get("/invoice/:id", verifyToken, async (req, res) => {
       const { id } = req.params;
       const result = await paymentCompleteCollection.findOne({ _id: id });
 
       res.send(result);
     });
 
-    app.get("/payment-history", async (req, res) => {
+    app.get("/payment-history", verifyToken, async (req, res) => {
       const email = req.query.email;
       const result = await paymentCompleteCollection.find({ email }).toArray();
       res.send(result);
     });
 
-    app.get("/payment-history-all", async (req, res) => {
-      const result = await paymentCompleteCollection
-        .find()
-        .sort({ orderDate: -1 })
-        .toArray();
-      res.send(result);
-    });
+    app.get(
+      "/payment-history-all",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const result = await paymentCompleteCollection
+          .find()
+          .sort({ orderDate: -1 })
+          .toArray();
+        res.send(result);
+      }
+    );
 
-    app.get("/seller-payment-history", async (req, res) => {
-      const sellerEmail = req.query.email;
-      const allPayments = await paymentCompleteCollection
-        .find()
-        .sort({ orderDate: -1 })
-        .toArray();
-      const sellerPayments = [];
-      allPayments.forEach((payment) => {
-        const sellerItems = payment.items.filter(
-          (item) => item.addedBy === sellerEmail
-        );
-        if (sellerItems.length > 0) {
-          sellerItems.forEach((item) => {
-            sellerPayments.push({
-              _id: payment._id,
-              transaction: payment.transaction,
-              orderDate: payment.orderDate,
-              status: payment.status,
-              buyerEmail: payment.email,
-              itemName: item.itemName,
+    app.get(
+      "/seller-payment-history",
+      verifyToken,
+      verifySeller,
+      async (req, res) => {
+        const sellerEmail = req.query.email;
+        const allPayments = await paymentCompleteCollection
+          .find()
+          .sort({ orderDate: -1 })
+          .toArray();
+        const sellerPayments = [];
+        allPayments.forEach((payment) => {
+          const sellerItems = payment.items.filter(
+            (item) => item.addedBy === sellerEmail
+          );
+          if (sellerItems.length > 0) {
+            sellerItems.forEach((item) => {
+              sellerPayments.push({
+                _id: payment._id,
+                transaction: payment.transaction,
+                orderDate: payment.orderDate,
+                status: payment.status,
+                buyerEmail: payment.email,
+                itemName: item.itemName,
+                quantity: item.quantity,
+                totalPrice: item.totalPrice,
+                item: item.itemName,
+              });
+            });
+          }
+        });
+        res.send(sellerPayments);
+      }
+    );
+
+    app.get(
+      "/admin-sales-report",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const payments = await paymentCompleteCollection
+          .find()
+          .sort({ orderDate: -1 })
+          .toArray();
+        const result = [];
+        payments.forEach((payment) => {
+          payment.items.forEach((item) => {
+            result.push({
+              medicineName: item.itemName,
+              sellerName: item.addedBy,
+              buyerName: payment.email,
               quantity: item.quantity,
               totalPrice: item.totalPrice,
-              item: item.itemName,
+              orderDate: payment.orderDate,
+              transaction: payment.transaction,
+              status: payment.status,
             });
           });
-        }
-      });
-      res.send(sellerPayments);
-    });
-
-    app.get("/admin-sales-report", async (req, res) => {
-      const payments = await paymentCompleteCollection
-        .find()
-        .sort({ orderDate: -1 })
-        .toArray();
-      const result = [];
-      payments.forEach((payment) => {
-        payment.items.forEach((item) => {
-          result.push({
-            medicineName: item.itemName,
-            sellerName: item.addedBy,
-            buyerName: payment.email,
-            quantity: item.quantity,
-            totalPrice: item.totalPrice,
-            orderDate: payment.orderDate,
-            transaction: payment.transaction,
-            status: payment.status,
-          });
         });
-      });
+        res.send(result);
+      }
+    );
+
+    app.get("/discount", async (req, res) => {
+      const result = await medicinesCollection
+        .find({ discount: { $gt: 0 } })
+        .toArray();
+      console.log(result);
       res.send(result);
     });
 
-    app.get("/all-users", async (req, res) => {
+    app.get("/all-users", verifyToken, verifyAdmin, async (req, res) => {
       const filter = {
         email: {
           $ne: req?.user?.email,
@@ -244,30 +359,35 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/seller-sales-revenue", async (req, res) => {
-      const sellerEmail = req.query.email;
-      const allPayments = await paymentCompleteCollection.find().toArray();
+    app.get(
+      "/seller-sales-revenue",
+      verifyToken,
+      verifySeller,
+      async (req, res) => {
+        const sellerEmail = req.query.email;
+        const allPayments = await paymentCompleteCollection.find().toArray();
 
-      let paidTotal = 0;
-      let pendingTotal = 0;
+        let paidTotal = 0;
+        let pendingTotal = 0;
 
-      allPayments.forEach((payment) => {
-        payment.items.forEach((item) => {
-          if (item.addedBy === sellerEmail) {
-            if (payment.status === "paid") {
-              paidTotal += item.totalPrice;
+        allPayments.forEach((payment) => {
+          payment.items.forEach((item) => {
+            if (item.addedBy === sellerEmail) {
+              if (payment.status === "paid") {
+                paidTotal += item.totalPrice;
+              }
+              if (payment.status === "pending") {
+                pendingTotal += item.totalPrice;
+              }
             }
-            if (payment.status === "pending") {
-              pendingTotal += item.totalPrice;
-            }
-          }
+          });
         });
-      });
-      res.send({
-        paidTotal: parseFloat(paidTotal.toFixed(2)),
-        pendingTotal: parseFloat(pendingTotal.toFixed(2)),
-      });
-    });
+        res.send({
+          paidTotal: parseFloat(paidTotal.toFixed(2)),
+          pendingTotal: parseFloat(pendingTotal.toFixed(2)),
+        });
+      }
+    );
 
     app.get("/admin-sales-revenue", async (req, res) => {
       const allPayments = await paymentCompleteCollection.find().toArray();
@@ -290,13 +410,26 @@ async function run() {
       });
     });
 
-    app.get("/manageCategory", async (req, res) => {
+    app.get("/manageCategory", verifyToken, verifyAdmin, async (req, res) => {
       const result = await categoryCollection
         .find()
         .sort({ createdAt: -1 })
         .toArray();
       res.send(result);
     });
+
+    app.get(
+      "/manageCategorySeller",
+      verifyToken,
+      verifySeller,
+      async (req, res) => {
+        const result = await categoryCollection
+          .find()
+          .sort({ createdAt: -1 })
+          .toArray();
+        res.send(result);
+      }
+    );
 
     app.get("/manageCategoryCard", async (req, res) => {
       const categories = await categoryCollection
